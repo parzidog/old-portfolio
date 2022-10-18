e128820dd62c92dd34c83f0cacdf23d31e6c0f6f
Kenenth Schaefer <kenneth.schaefer0@gmail.com>
2022-10-18 14:27:14 -0500

    Add database

--- server/db/database.js
+++ server/db/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/portfolio', { logging: false });
+const db = new Sequelize('postgres://localhost:5432/dcvmfbue3otak', { logging: false });
 
 module.exports = db;
 
